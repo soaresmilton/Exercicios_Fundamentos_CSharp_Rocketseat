0f3398b3c2722b9809101b0564f043ca208eb2b4
Milton Soares <eng.milton.soares@gmail.com>
2024-11-13 22:11:30 -0300

    first commit

---
+++ DesafioPratico01/Exercicios.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualBasic;
+
+namespace DesafioPratico01;
+
+public class Exercicios
+{
+    
+    public void Exercicio01()
+    {
+        string userName = Console.ReadLine();
+
+        Console.WriteLine($"Olá, {userName}! Seja muito bem-vindo!");
+    }
+
+    public void Exercicio02()
+    {
+        string userFirstName = Console.ReadLine();
+        string userLastName = Console.ReadLine();
+        Console.WriteLine($"{userFirstName} {userLastName}");
+    }
+
+    public (int sum, int subtraction, int multiplication, decimal division, int average)  Exercicio03(int val1, int val2)
+    {
+        var sum = val1 + val2;
+        var subtraction = val1 - val2;
+        var multiplication =  val1 * val2;
+        var division = (decimal) val1 / val2;
+        var average = (val1 + val2) / 2;
+        
+        return (sum, subtraction, multiplication, division, average);
+    }
+
+    public void Exercicio04()
+    {
+        string wordFromUser = Console.ReadLine();
+        
+        var counter = 0;
+        for(int i = 0; i < wordFromUser.Length; i++)
+        {
+            if (wordFromUser[i] is not ' ') counter++;
+        }
+
+        Console.WriteLine(counter);
+    }
+
+
+    public bool Exercicio05(string carPlate)
+    {
+        carPlate = carPlate.Trim().ToUpper();
+        if (carPlate.Length != 7) return false;
+
+        for (int i = 0; i < 3; i++)
+        {
+            if (!char.IsLetter(carPlate[i])) return false;
+        }
+
+        for (int i = 3; i < 7; i++)
+        {
+            if (!char.IsDigit(carPlate[i])) return false;
+        }
+
+        return true;
+    }
+
+    public void Exercicio06()
+    {
+        var now = DateTime.Now;
+        Console.WriteLine($"Formato completo: {now}");
+        Console.WriteLine($"No formato dd/mm/yyyy: {now.ToString("dd/mm/yyyy")}");
+        Console.WriteLine($"Formato completo: {now.ToString("HH")}");
+        Console.WriteLine($"Formato completo: {now.ToString("dd MMMM yyyy - HH:MM")}");
+    }
+}
---
+++ DesafioPratico01/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace DesafioPratico01;
+
+public class Program()
+{
+    static public void Main()
+    {
+        Exercicios exercicios = new Exercicios();
+        // exercicios.Exercicio01();
+        
+        // exercicios.Exercicio02();
+
+        /*
+         var result = exercicios.Exercicio03(val1: 1, val2: 2);
+         Console.WriteLine($"Valor da soma: {result.sum}\nValor da subtração {result.subtraction}\nValor da multiplicação: {result.multiplication}\nValor da divisão: {result.division}\nValor da média: {result.average}\n");
+        */
+
+        // exercicios.Exercicio04();
+
+        /*
+        string carPlate = Console.ReadLine();
+        var isPlateValid = exercicios.Exercicio05(carPlate: carPlate);
+        Console.WriteLine(isPlateValid);
+        */
+
+        // exercicios.Exercicio06();
+        
+    }
+}
